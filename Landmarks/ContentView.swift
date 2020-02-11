792adfccc6470fdd0b20d82b70d58a6f568e38bc
mwanjajoel <joelsilverworks@gmail.com>
2020-02-11 19:35:06 +0300

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Joel Mwanja on 11/02/2020.
+//  Copyright © 2020 Joel Mwanja. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
